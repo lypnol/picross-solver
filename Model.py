5aec50a3ca63d8d761401a561c8c0378ac56a16f
Corentin <corentin.dancette@student.ecp.fr>
2017-05-02 12:06:52 +0200

    add model coco

--- Model.py
+++ Model.py
@@ -1,97 +1,27 @@
 from utils import timed
+from ModelCoco import ModelCoco
+from BaseModel import ModelAyoub
 
 
 class UknownPicrossModel(Exception): pass
 
-class Model(object):
+class Model:
 
     MODEL_AYOUB = 0
+    MODEL_COCO = 1
 
     @staticmethod
     def get(model):
         if model == Model.MODEL_AYOUB:
             return ModelAyoub
+        elif model == Model.MODEL_COCO:
+            return ModelCoco
         else:
             raise UknownPicrossModel
 
     @staticmethod
     def all_models():
         return {
-            ModelAyoub.name(): Model.MODEL_AYOUB
+            ModelAyoub.name(): Model.MODEL_AYOUB,
+            ModelCoco.name(): Model.MODEL_COCO
         }
-
-    @staticmethod
-    def name():
-        pass
-
-    @staticmethod
-    def modelize(n, line_blocks, col_blocks):
-        pass
-
-    @staticmethod
-    def sat_solution_to_grid(n, n_var, solution, index):
-        pass
-
-
-class ModelAyoub(Model):
-
-    @staticmethod
-    def name():
-        return 'ayoub'
-
-    @staticmethod
-    @timed("ModelAyoub", "modelize")
-    def modelize(n, line_blocks, col_blocks):
-        n_var = 0
-        clauses = []
-
-        index = []
-        for i in range(n):
-            index.append([])
-            for j in range(n):
-                n_var += 1
-                index[i].append(n_var)
-
-        for i, block in enumerate(line_blocks):
-            if n in block:
-                for j in range(n):
-                    clauses.append([index[i][j]])
-            else:
-                for k in block:
-                    clauses.append([-index[i][j] for j in range(k)] + [-index[i][k]])
-                    clauses.append([-index[i][n-1-k]] + [-index[i][n-k+j] for j in range(k)])
-                    for j in range(1, n-k-1):
-                        clauses.append([-index[i][j+u] for u in range(k)] + [-index[i][j+k]])
-                        clauses.append([-index[i][j+u] for u in range(k)] + [-index[i][j-1]])
-                clauses.append([index[i][j] for j in range(n)])
-
-        for j, block in enumerate(col_blocks):
-            if n in block:
-                for i in range(n):
-                    clauses.append([index[i][j]])
-            else:
-                for k in block:
-                    clauses.append([-index[i][j] for i in range(k)] + [-index[k][j]])
-                    clauses.append([-index[n-1-k][j]] + [-index[n-k+i][j] for i in range(k)])
-                    for i in range(1, n-k-1):
-                        clauses.append([-index[i+u][j] for u in range(k)] + [-index[i+k][j]])
-                        clauses.append([-index[i+u][j] for u in range(k)] + [-index[i-1][j]])
-                clauses.append([index[i][j] for i in range(n)])
-
-        return n_var, clauses, index
-
-    @staticmethod
-    def sat_solution_to_grid(n, n_var, solution, index):
-        index = {}
-        var = 0
-        for i in range(n):
-            for j in range(n):
-                var += 1
-                index[var] = (i, j)
-
-        grid = [[0]*n]*n
-        for var in solution:
-            i, j = index[abs(var)]
-            grid[i][j] = 0 if var < 0 else 1
-
-        return grid
---
+++ ModelCoco.py
@@ -0,0 +1,220 @@
+from BaseModel import BaseModel
+from utils import timed
+import re
+
+class ModelCoco(BaseModel):
+
+    @staticmethod
+    def name():
+        return 'coco'
+
+    @staticmethod
+    @timed("ModelCoco", "modelize")
+    def modelize(n, line_blocks, col_blocks):
+        return picross_to_dimacs((n, line_blocks, col_blocks))
+
+    @staticmethod
+    def get_coords_from_string(str):
+        reg = 'x_(.)_(.)'
+        m = re.match(reg, str)
+        if m:
+            i, j = [int(x) for x in m.groups()]
+            return i, j
+
+    @staticmethod
+    @timed("ModelCoco", "sat_solution_to_grid")
+    def sat_solution_to_grid(n, n_var, solution, index):
+
+        grille_sol = [[0 for _ in range(n)] for _2 in range(n)]
+
+        for v in solution:
+            var_name = index[abs(v)]
+            if 'x' in var_name:
+                i, j = ModelCoco.get_coords_from_string(var_name)
+                grille_sol[i][j] = 1 if v > 0 else 0
+        return grille_sol
+
+class And(list):
+    def __repr__(self):
+        return "(" + " ∧ ".join(str(x) for x in self) + ")"
+
+class Or(list):
+    def __repr__(self):
+        return "(" + " ∨ ".join(str(x) for x in self) + ")"
+
+class Literal:
+    def __init__(self, name, truth=True):
+        self.name = str(name)
+        self.truth = truth
+
+    def to_string(self):
+        truth = "" if self.truth else "-"
+        return truth + self.name
+
+    def __str__(self):
+        return self.to_string()
+
+    def __repr__(self):
+        return self.to_string()
+
+    def inverted(self):
+        return Literal(self.name, not self.truth)
+
+    def __eq__(self, other):
+        return self.name == other.name and self.truth == other.truth
+
+assert (Literal('x') == Literal('x'))
+assert (Literal('x')) != Literal('x', False)
+assert (Literal('x')) != Literal('y')
+assert (Literal('x') == Literal('x', False).inverted())
+
+class PicrossLiteral(Literal):
+
+    def __init__(self, i, j, truth):
+        self.i = i
+        self.j = j
+        self.truth = truth
+
+    @property
+    def name(self):
+        return "x_{i}_{j}".format(
+            i=self.i,
+            j=self.j
+        )
+
+    def inverted(self):
+        return PicrossLiteral(self.i, self.j, not self.truth)
+
+def get_all_possible_rec(current, restants, size):
+    if restants == []:
+        return [current + [0] * (size - len(current))]
+    a = restants[0]
+    i = len(current)  # position courante
+    possibles = []
+    j = 0  # combien de 0 avant d'ajouter l'élément
+    while i + j + a <= size:
+        p = current + [0] * j + [1] * a
+        if len(p) < size:
+            p.append(0)
+        possibles.append(p)
+        j += 1
+    result = []
+    for p in possibles:
+        r = get_all_possible_rec(p, restants[1:], size)
+        result.extend(r)
+    return result
+
+def get_all_possible(coefficients, size):
+    return get_all_possible_rec([], coefficients, size)
+
+def convert_to_dnf(possibles, line=None, column=None):
+    if line is None and column is None:
+        raise Exception("no line or column")
+    return Or([
+        And([
+            PicrossLiteral(line if line is not None else j,
+                           column if column is not None else j,
+                           truth=bool(x))
+            for j, x in enumerate(p)
+        ])
+        for p in possibles
+    ])
+
+a = get_all_possible_rec([], [1, 1], 5)
+
+def get_all_dnfs(picross):
+    formulas = {}
+    size, lines, columns = picross
+    for i, l in enumerate(lines):
+        formulas["l" + str(i)] = convert_to_dnf(get_all_possible(l, size), line=i)
+
+    for i, c in enumerate(columns):
+        formulas["c" + str(i)] = convert_to_dnf(get_all_possible(c, size), column=i)
+
+    return formulas
+
+def dnf_to_cnf(dnf, id):
+    """
+    prend une dnf sous la forme d'une liste de liste de literaux
+    ie 
+    :return: meme format, en CNF
+    """
+    n = len(dnf)
+    l = Literal(id, True)
+
+    # variables pour chaque ligne / colonne
+    new_variables = [Literal("{id}_{i}".format(id=id, i=i)) for i in range(n)]
+
+    result = And([])
+    # first global literal
+    result.append(Or([l]))
+
+    # l => l1 v l1 v ...
+    result.append(Or([l.inverted()] + new_variables))
+    # l <= l1 v l2 ... == (l v -l1) ^ (l v -l2) ...
+    for v in new_variables:
+        result.append(Or([l, v.inverted()]))
+
+    # specific clauses
+    for i in range(n):
+        l_i = new_variables[i]
+        # l_i <=> dnf[i]
+        # revient à
+        # l_i => dnf[i] == -l_i v (x1 ^ x2 ^ ..)
+        result.append(Or([l_i] + [x.inverted() for x in dnf[i]]))
+        for v in dnf[i]:
+            result.append(Or([l_i.inverted(), v]))
+    return result
+
+def get_all_cnfs(dnfs):
+    cnfs = {}
+    for line in dnfs:
+        cnfs[line] = dnf_to_cnf(dnfs[line], line)
+    return cnfs
+
+def flatten_cnfs(cnfs):
+    result = And([])
+    for key in cnfs:
+        result.extend(cnfs[key])
+    return result
+
+def cnf_to_dimacs(cnf):
+    # list variables
+    variables = []
+    for clause in cnf:
+        for lit in clause:
+            if lit.name not in variables:
+                variables.append(lit.name)
+    # put ids in dict
+    variables_dict = {}
+    for i, v in enumerate(variables):
+        variables_dict[v] = i + 1
+
+    n_var = len(variables)
+    clauses_dimacs = []
+
+    # to dimacs
+    dimacs = "p cnf {n_var} {n_clause}\n".format(n_var=len(variables), n_clause=len(cnf))
+    for clause in cnf:
+        line_dimacs = []
+        for lit in clause:
+            var = (1 if lit.truth else -1) * variables_dict[lit.name]
+            line_dimacs.append(var)
+        clauses_dimacs.append(line_dimacs)
+    return n_var, clauses_dimacs, [None] + variables
+
+
+def picross_to_dimacs(picross):
+    return cnf_to_dimacs(flatten_cnfs(get_all_cnfs(get_all_dnfs(picross))))
+
+def output_to_result(output, variable):
+    vars = output.split()
+
+    for v in vars:
+        if v == "0":
+            break
+        if v.startswith("-"):
+            print(variable[int(v[1:])] + " : false")
+        else:
+            print(variable[int(v)] + " : true")
+
